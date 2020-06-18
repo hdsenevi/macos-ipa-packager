a1041736a4454bff728f0826c65845837959745d
Shanaka Senevirathne <shanaka.gunasekarasenevirathne@nab.com.au>
2020-06-18 12:48:03 +1000

    Initial Commit

---
+++ ipa-packager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ipa-packager
+//
+//  Created by Shanaka Senevirathne on 18/6/20.
+//  Copyright © 2020 shanaka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ipa-packager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ipa-packager
+//
+//  Created by Shanaka Senevirathne on 18/6/20.
+//  Copyright © 2020 shanaka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
